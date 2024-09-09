1c4d07a95fdaab8436ec190d1046bd909c8c33c6
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-09-09 15:35:03 +0100

    Initial Setup. KafkaConsumer with Config

---
+++ src/test/java/com/microservices/demo/cloudstreamkafkaplayground/CloudStreamKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.cloudstreamkafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamKafkaPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
