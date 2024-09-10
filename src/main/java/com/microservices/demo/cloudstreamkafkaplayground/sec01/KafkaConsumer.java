d770c9d65d2e698d8cf7afa5b54237e7a681c682
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-09-10 09:48:46 +0100

    1. Receiver Options Customizer

--- src/main/java/com/microservices/demo/cloudstreamkafkaplayground/sec01/KafkaConsumer.java
+++ src/main/java/com/microservices/demo/cloudstreamkafkaplayground/sec01/KafkaConsumer.java
@@ -1,7 +1,9 @@
 package com.microservices.demo.cloudstreamkafkaplayground.sec01;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.binder.reactorkafka.ReceiverOptionsCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
@@ -13,6 +15,14 @@ import java.util.function.Function;
 @Configuration
 public class KafkaConsumer {
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @Bean
+    public ReceiverOptionsCustomizer<String, String> customizer(){
+        return (s, ro) ->{
+            log.info("******************************** {}", s);
+           return ro.consumerProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "234");
+        };
+    }
 
     @Bean
     public Consumer<Flux<String>> consumer(){
