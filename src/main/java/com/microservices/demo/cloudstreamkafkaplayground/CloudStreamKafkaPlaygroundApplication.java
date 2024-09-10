f9ef8ea107653186a2445beba197d057b39cf7f0
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-09-10 09:01:04 +0100

    1. Refactor for Active Profiles

--- src/main/java/com/microservices/demo/cloudstreamkafkaplayground/CloudStreamKafkaPlaygroundApplication.java
+++ src/main/java/com/microservices/demo/cloudstreamkafkaplayground/CloudStreamKafkaPlaygroundApplication.java
@@ -3,7 +3,7 @@ package com.microservices.demo.cloudstreamkafkaplayground;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.microservices.demo.cloudstreamkafkaplayground.${sec}")
 public class CloudStreamKafkaPlaygroundApplication {
 
     public static void main(String[] args) {
