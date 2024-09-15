5f65664603ed3c14336cdd243167186d04fa56c1
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-09-15 15:11:38 +0100

    Native Encoding and Decoding Using yaml file deserializers 1. Decoding Using Abstract Types - Part 1 2. Part 2 Using AbstractType 3. Using common Bindings with Default Bindings

--- src/main/java/com/microservices/demo/cloudstreamkafkaplayground/sec11/KafkaConsumer.java
+++ src/main/java/com/microservices/demo/cloudstreamkafkaplayground/sec11/KafkaConsumer.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Consumer;
@@ -14,9 +15,11 @@ public class KafkaConsumer {
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
     @Bean
-    public Consumer<Flux<ContactMethod>> consumer() {
+    public Consumer<Flux<Message<ContactMethod>>> consumer() {
         return flux ->
                 flux.doOnNext(s -> log.info("Consumer received {}", s))
+                        // the typeId is sent with the message.
+                        .doOnNext(s -> log.info(new String((byte[]) s.getHeaders().get("__TypeId__"))))
                         .subscribe();
     }
 }
